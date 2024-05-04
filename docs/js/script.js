a9e491b7f5eafc7412f37be6743de7cff7c038ab
Avihai Ben-David <bd.avihai@gmail.com>
2024-05-04 17:33:22 +0300

    back to html links

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,7 +2,7 @@
 document.querySelectorAll('.sidebar a').forEach(menuItem => {
   menuItem.addEventListener('click', (event) => {
     event.preventDefault(); // Prevent default anchor link behavior
-    const contentUrl = menuItem.getAttribute('href'); // Extract markdown filename
+    const contentUrl = menuItem.getAttribute('href').slice(1) + '.html'; // Extract markdown filename
 
     fetch(contentUrl)
       .then(response => response.text())
@@ -17,7 +17,7 @@ document.querySelectorAll('.sidebar a').forEach(menuItem => {
 $(document).ready(function() {
   $('.sidebar a').click(function(event) {
     event.preventDefault();
-    var contentUrl = $(this).attr('href');
+    var contentUrl = $(this).attr('href').slice(1) + '.html';
 
     $.get(contentUrl, function(data) {
       $('.content').html(data);
