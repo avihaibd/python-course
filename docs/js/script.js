d917c4130b09fe9d12449220da7aedebc186474e
Avihai Ben-David <bd.avihai@gmail.com>
2024-05-04 18:16:41 +0300

    js fixes

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,25 +1,31 @@
 // Using fetch API
-document.querySelectorAll('.sidebar a').forEach(menuItem => {
-  menuItem.addEventListener('click', (event) => {
-    event.preventDefault(); // Prevent default anchor link behavior
-    const contentUrl = menuItem.getAttribute('href').replace('.md', '.html'); // Replace .md with .html
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll('.sidebar a').forEach(menuItem => {
+    menuItem.addEventListener('click', (event) => {
+      event.preventDefault(); // Prevent default anchor link behavior
+      const contentUrl = menuItem.getAttribute('href').replace('.md', '.html'); // Replace .md with .html
 
-    fetch(contentUrl)
-      .then(response => response.text())
-      .then(data => {
-        const iframe = document.querySelector('.content iframe');
-        const iframeDocument = iframe.contentWindow.document; // Access the iframe document
+      fetch(contentUrl)
+        .then(response => response.text())
+        .then(data => {
+          const iframe = document.querySelector('.content iframe');
+          if (iframe) {
+            const iframeDocument = iframe.contentWindow.document; // Access the iframe document
 
-        // Set attributes of the HTML content inside the iframe
-        iframeDocument.documentElement.setAttribute('dir', 'rtl'); // Set text direction to right-to-left
-        iframeDocument.documentElement.setAttribute('lang', 'he'); // Set language to Hebrew
+            // Set attributes of the HTML content inside the iframe
+            iframeDocument.documentElement.setAttribute('dir', 'rtl'); // Set text direction to right-to-left
+            iframeDocument.documentElement.setAttribute('lang', 'he'); // Set language to Hebrew
 
-        // Inject the fetched data into the iframe document
-        iframeDocument.open();
-        iframeDocument.write(data);
-        iframeDocument.close();
-      })
-      .catch(error => console.error(error));
+            // Inject the fetched data into the iframe document
+            iframeDocument.open();
+            iframeDocument.write(data);
+            iframeDocument.close();
+          } else {
+            console.error('Iframe element not found.');
+          }
+        })
+        .catch(error => console.error(error));
+    });
   });
 });
 
